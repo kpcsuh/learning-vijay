bbb7ac37bb5a1d2bf1680f125ea44e4fbb394df2
Prasad Kakarlamudi <kpcsuh@gmail.com>
2025-04-16 17:18:19 -0500

    First Commit
    First commit

---
+++ src/main/java/com/example/learning_vijay/LearningVijayApplication.java
@@ -0,0 +1,13 @@
+package com.example.learning_vijay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningVijayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningVijayApplication.class, args);
+	}
+
+}
